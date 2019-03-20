d72185653321db4b813f9191d30265576ab79a1a
Isaac Griffith <isaacgriffith@gmail.com>
2019-03-20 11:12:00 -0600

    updated license headers

--- src/test/groovy/MetricTests.groovy
+++ src/test/groovy/MetricTests.groovy
@@ -2,8 +2,9 @@
  * The MIT License (MIT)
  *
  * MSUSEL Metrics
- * Copyright (c) 2015-2018 Montana State University, Gianforte School of Computing,
- * Software Engineering Laboratory
+ * Copyright (c) 2015-2019 Montana State University, Gianforte School of Computing,
+ * Software Engineering Laboratory and Idaho State University, Informatics and
+ * Computer Science, Empirical Software Engineering Laboratory
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -23,7 +24,6 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
  */
-
 import edu.montana.gsoc.msusel.codetree.CodeTree
 import edu.montana.gsoc.msusel.codetree.node.structural.Project
 import edu.montana.gsoc.msusel.codetree.node.type.Type
