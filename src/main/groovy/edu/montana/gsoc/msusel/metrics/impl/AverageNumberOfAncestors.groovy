c5f70a0b1e2b1bd43c087265013e93fb9d540cf4
Isaac Griffith <isaacgriffith@gmail.com>
2021-05-20 13:58:36 -0600

    updated

--- src/main/groovy/edu/montana/gsoc/msusel/metrics/impl/AverageNumberOfAncestors.groovy
+++ src/main/groovy/edu/montana/gsoc/msusel/metrics/impl/AverageNumberOfAncestors.groovy
@@ -75,7 +75,7 @@ class AverageNumberOfAncestors extends MetricEvaluator {
             Map<Type, Integer> map = new HashMap<>()
             def classes = struct.getAllTypes()
             classes.each { Type t ->
-                total += Measure.valueFor("arc-metrics", "NOA", t)
+                total += Measure.valueFor("arc_metrics", "NOA", t)
             }
             if (classes)
                 total /= classes.size()
